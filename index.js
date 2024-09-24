a94d074d043a5a84e71ac778b36dac94c188b811
jainoddin <skjainoddin39854@gmail.com>
2024-09-24 22:07:08 +0530

    Your commit message

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-   
-   var array=["jain","mom","nishu","baba"]
+var a = "jaInOddiN";
+var b=a.split('').reverse().join(''); ;
+console.log("reavce",b);
 
-   console.log(array)
-   
 
-   function test(){
-    var x=20;
-    
-
+for (var i = 0; i < a.length; i++) {
+   var currentChar = a[i]; // Get the character at the current index
+   if (currentChar === currentChar.toUpperCase()) {
+      console.log("Capital letter:", currentChar);
+   } else if (currentChar === currentChar.toLowerCase()) {
+      console.log("Small letter:", currentChar);
    }
-   console.log(test())
-   
+}
 
 
 
 
+
+for (var i = 'a'; i < 'z'; i++) {
+   if(a[i]==='a'&&a[i]==='z'){
+      console.log('aaa',a[i])
+   }
+}
